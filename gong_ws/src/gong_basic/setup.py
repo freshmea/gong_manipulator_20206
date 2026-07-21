f5c5775c59a25646655ec11ea2d0142472942da0
freshmea <freshmea@naver.com>
2026-07-21 10:56:41 +0900

    gong pakage

---
+++ gong_ws/src/gong_basic/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'gong_basic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='choi su gil',
+    maintainer_email='freshmea@naver.com',
+    description='gongju university ROS2 basic library',
+    license='Apache 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "simple_pub = gong_basic.simple_pub:main"
+        ],
+    },
+)
