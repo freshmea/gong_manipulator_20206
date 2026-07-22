41f6a3091a09f9a7438685e36d4cd2d3df7b771a
freshmea <freshmea@naver.com>
2026-07-22 16:07:40 +0900

    parameter

--- gong_ws/src/gong_basic/gong_basic/my_param.py
+++ gong_ws/src/gong_basic/gong_basic/my_param.py
@@ -1,5 +1,7 @@
 import rclpy
+from rcl_interfaces.msg import SetParametersResult
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 
 
 class TParam(Node):
@@ -9,9 +11,16 @@ class TParam(Node):
         self.declare_parameter("my_param", "내가 만든 클래스 노드 안의 파라미터")
         self.my_param = self.get_parameter("my_param").get_parameter_value().string_value
         self.create_timer(1, self.timer_callback)
+        self.add_on_set_parameters_callback(self.parameter_callback)
 
     def timer_callback(self):
         self.get_logger().info(self.my_param)
+
+    def parameter_callback(self, params: list[Parameter]):
+        for param in params:
+            if param.name == "my_param":
+                self.my_param = param.value
+        return SetParametersResult(successful=True)
 
 
 def main(args=None):
