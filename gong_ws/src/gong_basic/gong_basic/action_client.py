a893bfa0bfc85d5eebe61754cd4f22af504b75ef
freshmea <freshmea@naver.com>
2026-07-23 13:58:00 +0900

    action client

--- gong_ws/src/gong_basic/gong_basic/action_client.py
+++ gong_ws/src/gong_basic/gong_basic/action_client.py
@@ -1,14 +1,14 @@
 # ros2 action send_goal /fibonacci_server user_interface/action/Fibonacci "{step: 10}" --feedback
-import time
+import sys
 
 import rclpy
 from action_msgs.msg import GoalStatus
 from rclpy.action import ActionClient
 from rclpy.action.client import ClientGoalHandle
-from rclpy.action.server import ServerGoalHandle
 from rclpy.node import Node
 from rclpy.task import Future
 from user_interface.action import Fibonacci
+from user_interface.action._fibonacci import Fibonacci_GetResult_Response
 
 
 class Action_client(Node):
@@ -22,9 +22,10 @@ class Action_client(Node):
 
     def send_goal(self, step: str):
         goal_msg = Fibonacci.Goal()
-        goal_msg.step = step
+        goal_msg.step = int(step)
         # 서버에 접속()
-        self.action_client.wait_for_server(timeout_sec=1)
+        while not self.action_client.wait_for_server(timeout_sec=1):
+            self.get_logger().info("fibonacci server is not available!!")
         # request 보내기 -> goal 보내기
         self.future = self.action_client.send_goal_async(
             goal_msg, feedback_callback=self.feedback_callback
@@ -33,23 +34,37 @@ class Action_client(Node):
 
     def goal_response_callback(self, future: Future):
         # 첫 goal 을 주고 난 다음의 response
-
-        # result 가 왔을때의 callback 을 등록
         goal_handle: ClientGoalHandle = future.result()  # type: ignore
+        if not goal_handle.accepted:
+            self.get_logger().info("goal rejected!!")
+            return
+        self.get_logger().info("goal accepted!!")
+        # result 가 왔을때의 callback 을 등록
         self.get_result_future = goal_handle.get_result_async()
         self.get_result_future.add_done_callback(self.get_result_callback)
-        pass
+        self.get_logger().info("end of goal response callback function!!")
 
-    def feedback_callback(self, mgs: Fibonacci.Feedback):
-        pass
+    def feedback_callback(self, msg: Fibonacci.Impl.FeedbackMessage):
+        feedback: Fibonacci.Feedback = msg.feedback
+        self.get_logger().info(f"{list(feedback.temp_seq)}")
 
     def get_result_callback(self, future: Future):
-        pass
+        result: Fibonacci_GetResult_Response = future.result()  # type : ignore
+        if result.status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info(f"result: {list(result.result.seq)}")
+        if result.status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().info("aborted!!")
+        if result.status == GoalStatus.STATUS_CANCELED:
+            self.get_logger().info("canceled!!")
 
 
 def main(args=None):
     rclpy.init(args=args)  # rmw 활성화
     node = Action_client()
+    if len(sys.argv) != 2:
+        print("사용법 : ros2 run [package] [node] [step: int]")
+        return
+    node.send_goal(sys.argv[1])
     try:
         rclpy.spin(node)  # 블럭 (무한 루프)
     except KeyboardInterrupt:
