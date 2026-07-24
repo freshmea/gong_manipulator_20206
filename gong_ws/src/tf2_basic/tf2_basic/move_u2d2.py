93b204a07430ff03ebe7a392f31565e868917ffc
freshmea <freshmea@naver.com>
2026-07-24 16:34:50 +0900

    과제

---
+++ gong_ws/src/tf2_basic/tf2_basic/move_u2d2.py
@@ -0,0 +1,5 @@
+# 팔을 추가해서 엘보우 구현(revolute)
+# 05_add_arm.urdf
+# 실행 ros2 launch tf2_basic urdf_display.launch.py model:=urdf/05_add_arm.urdf
+# tf 를 발행해서 머리를 돌리거나 막대기를 꺼나, 바퀴를 굴려보세요. 재미있게 동작하게
+# ros2 run tf2_basic move_u2d2 로 움직이기
