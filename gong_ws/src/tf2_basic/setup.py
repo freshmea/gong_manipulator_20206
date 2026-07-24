6ea1676c0ed2eb893b331833b249061ca98f4671
freshmea <freshmea@naver.com>
2026-07-24 14:31:27 +0900

    urdf display

--- gong_ws/src/tf2_basic/setup.py
+++ gong_ws/src/tf2_basic/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = "tf2_basic"
@@ -9,6 +12,10 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob(os.path.join("launch", "*.launch.py"))),
+        ("share/" + package_name + "/urdf", glob(os.path.join("urdf", "*.*"))),
+        ("share/" + package_name + "/rviz", glob(os.path.join("rviz", "*.*"))),
+        ("share/" + package_name + "/meshes", glob(os.path.join("meshes", "*.*"))),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
